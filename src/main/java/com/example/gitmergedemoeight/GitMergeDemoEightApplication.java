a6822c939bce0065f12b08e22bae33be2d8da3cb
王钰 <wangyu9@bonc.com.cn>
2022-01-18 21:21:40 +0800

    master1:master1分支提交

--- src/main/java/com/example/gitmergedemoeight/GitMergeDemoEightApplication.java
+++ src/main/java/com/example/gitmergedemoeight/GitMergeDemoEightApplication.java
@@ -16,6 +16,7 @@ public class GitMergeDemoEightApplication {
         System.out.println("222");
         System.out.println("3333");
         System.out.println("4444");
+        System.out.println("7777");
         System.out.println("6666");
     }
     public void helloOne(){
