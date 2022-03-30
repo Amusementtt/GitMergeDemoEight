d6b1a16fbd4964ddb0b8827fc24d5f0515a784f8
王钰 <wangyu9@bonc.com.cn>
2022-03-30 13:19:41 +0800

    master:master分支提交

--- src/main/java/com/example/gitmergedemoeight/GitMergeDemoEightApplication.java
+++ src/main/java/com/example/gitmergedemoeight/GitMergeDemoEightApplication.java
@@ -92,5 +92,6 @@ public class GitMergeDemoEightApplication {
         System.out.println("333");
         System.out.println("666");
         System.out.println("555");
+        System.out.println("888");
     }
 }
